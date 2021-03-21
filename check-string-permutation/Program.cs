7132055e8c98769f9f9b07f79bbc2a86c7f61f53
Burak Eker <mehmetburakeker@gmail.com>
2021-03-21 22:32:36 +0300

    Add project files

---
+++ check-string-permutation/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace check_string_permutation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(StringHelper.CheckPermutation("baba", "abab"));
+            Console.WriteLine(StringHelper.CheckPermutation("baba", "abc"));
+            Console.WriteLine(StringHelper.CheckPermutation("lds", "loodos"));
+        }
+    }
+}
---
+++ check-string-permutation/StringHelper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace check_string_permutation
+{
+    public static class StringHelper
+    {
+        public static bool CheckPermutation(string firstWord, string secondWord)
+        {
+            var firstWordDict = firstWord.ToCharCountKeyValuePair();
+            var secondWordDict = secondWord.ToCharCountKeyValuePair();
+
+            var oneOfThePermutationOfFirstWordEqualsToSecondWord = true;
+            foreach (var (key, val) in firstWordDict)
+            {
+                try
+                {
+                    if (secondWordDict[key] < firstWordDict[key])
+                    {
+                        oneOfThePermutationOfFirstWordEqualsToSecondWord = false;
+                    }
+                }
+                catch (KeyNotFoundException)
+                {
+                    oneOfThePermutationOfFirstWordEqualsToSecondWord = false;
+                }
+            }
+
+            var oneOfThePermutationOfSecondWordEqualsToFirstWord = true;
+            foreach (var (key, val) in secondWordDict)
+            {
+                try
+                {
+                    if (firstWordDict[key] < secondWordDict[key])
+                    {
+                        oneOfThePermutationOfSecondWordEqualsToFirstWord = false;
+                    }
+                }
+                catch (KeyNotFoundException)
+                {
+                    oneOfThePermutationOfSecondWordEqualsToFirstWord = false;
+                }
+            }
+
+            return oneOfThePermutationOfFirstWordEqualsToSecondWord || oneOfThePermutationOfSecondWordEqualsToFirstWord;
+        }
+        public static Dictionary<char, int> ToCharCountKeyValuePair(this string str)
+        {
+            var charCountKvp = new Dictionary<char, int>();
+            foreach (char character in str)
+            {
+                if (!charCountKvp.ContainsKey(character))
+                {
+                    charCountKvp[character] = 0;
+                }
+                charCountKvp[character] += 1;
+            }
+            return charCountKvp;
+        }
+    }
+}
